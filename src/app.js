20d899820cdd1ddb611dd2c5e2bcbf7da1afed0c
Steve Navarro <franconz17@icloud.com>
2024-01-07 11:24:54 -0500

    Proyecto iniciado

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from './components/navbar'
+
+function App() {
+    return(
+
+    );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+function NavBar(params) {
+    return (
+
+    );
+}
+
+export default NavBar;
